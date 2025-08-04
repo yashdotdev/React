9e28b826f9aa5b527c9c444d67ed87adef2ffa4c
yashdotdev <thisisyash459@gmail.com>
2025-08-04 13:43:21 +0530

    indentation fixed

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -2,13 +2,9 @@ import { useState, useCallback, useEffect, useRef } from "react";
 
 function App() {
   const [length, setLength] = useState(8);
-
   const [numberAllowed, setNumberAllowed] = useState(false);
-
   const [charAllowed, setCharAllowed] = useState(false);
-
   const [password, setPassword] = useState("");
-
   const passwordRef = useRef(null);
 
   const passwordGenerator = useCallback(() => {
