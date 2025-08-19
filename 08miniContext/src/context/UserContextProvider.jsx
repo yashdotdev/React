cfbd33c916136ea9f4494b63d065916751a76891
yashdotdev <thisisyash459@gmail.com>
2025-08-19 19:28:43 +0530

    fixed indentatio

--- 08miniContext/src/context/UserContextProvider.jsx
+++ 08miniContext/src/context/UserContextProvider.jsx
@@ -4,13 +4,13 @@ import UserContext from "./UserContext";
 
 // Part-2 (Setting up the UserContextProvider to provide the access of all the componenets)
 
-const UserContextProvider = ({children}) => {
-    const [user, setUser] = React.useState(null);
-    return(
-        <UserContext.Provider value={{user, setUser}}>
-            {children}
-        </UserContext.Provider>
-    )
-}
+const UserContextProvider = ({ children }) => {
+  const [user, setUser] = React.useState(null);
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
 
 export default UserContextProvider;
